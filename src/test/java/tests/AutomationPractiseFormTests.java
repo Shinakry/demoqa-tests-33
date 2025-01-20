718f9030c5a3ac0913b09b61587ebfd2a132406d
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-21 00:04:51 +0500

    -add form state and city

--- src/test/java/tests/AutomationPractiseFormTests.java
+++ src/test/java/tests/AutomationPractiseFormTests.java
@@ -30,20 +30,25 @@ public class AutomationPractiseFormTests {
         $("#firstName").setValue("testFirstName"); //заполнили имя
         $("#lastName").setValue("testLastName"); //заполнили фамилию
         $("#userEmail").setValue("testUserEmail@test.ru"); //заполнили фамилию
-        $(byText("Other")).click();
-        $(".mr-sm-2.form-control#userNumber").setValue("8912251475");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("3");
-        $(".react-datepicker__year-select").selectOptionByValue("1939");
-        $(".react-datepicker__day--012").click();
-        $(".subjects-auto-complete__control").click();
-        $("#subjectsInput").setValue("m");
-        $("#react-select-2-option-1").click();
-        $(byText("Sports")).click();
+        $(byText("Other")).click(); //пол
+        $(".mr-sm-2.form-control#userNumber").setValue("8912251475"); //номер телефона
+        $("#dateOfBirthInput").click(); //дата рождения
+        $(".react-datepicker__month-select").selectOptionByValue("3"); //выбор месяца для др
+        $(".react-datepicker__year-select").selectOptionByValue("1939"); //выбор года для др
+        $(".react-datepicker__day--012").click(); //выбор даты для др
+        $(".subjects-auto-complete__control").click(); //выбор предметов
+        $("#subjectsInput").setValue("m"); //символ/слово, которое вводим
+        $("#react-select-2-option-1").click(); //порядковый номер предмета, который выбираем
+        $(byText("Sports")).click(); //выбор хобби
         $(byText("Reading")).click();
         $(byText("Music")).click();
-        $("#uploadPicture").uploadFile(uploadFile);
-        $("#currentAddress").setValue("currentAddressTest");
-        $("#state").scrollTo().click();
+        $("#uploadPicture").uploadFile(uploadFile); //загрузка картинки
+        $("#currentAddress").setValue("currentAddressTest"); //заполнение формы с адресом
+        $("#state").scrollTo().click(); //скролл до элемента Штат и клик
+        $("#react-select-3-option-3").click(); //порядковый номер из выподающего списка по штатам
+        $("#city").click(); //раскрытие списка по городам
+        $("#react-select-4-option-0").click(); //выбор города из выпадающего списка
+
+
     }
 }
