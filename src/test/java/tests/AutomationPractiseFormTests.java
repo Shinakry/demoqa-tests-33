414627243ea6c14ca5feb15847d5bf1fb15fb612
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-22 20:39:17 +0500

    -correction of comments

--- src/test/java/tests/AutomationPractiseFormTests.java
+++ src/test/java/tests/AutomationPractiseFormTests.java
@@ -9,8 +9,7 @@ import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPractiseFormTests {
 
@@ -19,63 +18,50 @@ public class AutomationPractiseFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
     }
 
     @Test
     void allFormTest(){
-        File uploadFile = new File("src/test/resources/photo_2024-01-11_13-55-58.jpg");
         String nameStudent = new String("testFirstName");
         String lastNameStudent = new String("testLastName");
 
         open("/automation-practice-form");
-        $("#firstName").setValue(nameStudent); //заполнили имя
-        $("#lastName").setValue(lastNameStudent); //заполнили фамилию
-        $("#userEmail").setValue("testUserEmail@test.ru"); //заполнили фамилию
-        $(byText("Other")).click(); //пол
-        $(".mr-sm-2.form-control#userNumber").setValue("8912251475"); //номер телефона
-        $("#dateOfBirthInput").click(); //дата рождения
-        $(".react-datepicker__month-select").selectOptionByValue("3"); //выбор месяца для др
-        $(".react-datepicker__year-select").selectOptionByValue("1939"); //выбор года для др
-        $(".react-datepicker__day--012").click(); //выбор даты для др
-        $(".subjects-auto-complete__control").click(); //выбор предметов
-        $("#subjectsInput").setValue("m"); //символ/слово, которое вводим
-        $("#react-select-2-option-1").click(); //порядковый номер предмета, который выбираем
-        $(byText("Sports")).click(); //выбор хобби
-        $(byText("Reading")).click();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFile(uploadFile); //загрузка картинки
-        $("#currentAddress").setValue("currentAddressTest"); //заполнение формы с адресом
-        $("#state").scrollTo().click(); //скролл до элемента Штат и клик
-        $("#react-select-3-option-3").click(); //порядковый номер из выподающего списка по штатам
-        $("#city").click(); //раскрытие списка по городам
-        $("#react-select-4-option-0").click(); //выбор города из выпадающего списка
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue(nameStudent);
+        $("#lastName").setValue(lastNameStudent);
+        $("#userEmail").setValue("testUserEmail@test.ru");
+        $(byText("Other")).click();
+        $(".mr-sm-2.form-control#userNumber").setValue("8912251475");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("3");
+        $(".react-datepicker__year-select").selectOptionByValue("1939");
+        $(".react-datepicker__day--012").click();
+        $(".subjects-auto-complete__control").click();
+        $("#subjectsInput").setValue("m");
+        $("#react-select-2-option-1").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("photo_2024-01-11_13-55-58.jpg");
+        $("#currentAddress").setValue("currentAddressTest");
+        $("#state").scrollTo().click();
+        $("#react-select-3-option-3").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
 
-        //отправка данных, получение результирующей таблицы
         $("#submit").click();
 
-        //проверка наличия всех полей в результирующей таблице
-        $(".modal-content").shouldHave(text("testFirstName testLastName"));
-        $(".modal-content").shouldHave(text("Student Email"));
-        $(".modal-content").shouldHave(text("Gender"));
-        $(".modal-content").shouldHave(text("Mobile"));
-        $(".modal-content").shouldHave(text("Date of Birth"));
-        $(".modal-content").shouldHave(text("Subjects"));
-        $(".modal-content").shouldHave(text("Hobbies"));
-        $(".modal-content").shouldHave(text("Picture"));
-        $(".modal-content").shouldHave(text("Address"));
-        $(".modal-content").shouldHave(text("State and City"));
-
-        //проверка корректности наполнения полей в результирующей таблице
-        $(".modal-content").shouldHave(text(nameStudent + (" ") + lastNameStudent));
-        $(".modal-content").shouldHave(text("testUserEmail@test.ru"));
-        $(".modal-content").shouldHave(text("Other"));
-        $(".modal-content").shouldHave(text("8912251475"));
-        $(".modal-content").shouldHave(text("12 April,1939"));
-        $(".modal-content").shouldHave(text("Chemistry"));
-        $(".modal-content").shouldHave(text("Sports, Reading, Music"));
-        $(".modal-content").shouldHave(text("photo_2024-01-11_13-55-58.jpg"));
-        $(".modal-content").shouldHave(text("currentAddressTest"));
-        $(".modal-content").shouldHave(text("Rajasthan Jaipur"));
+        $(".modal-content").shouldHave(text("testFirstName testLastName")).parent().shouldHave(text(nameStudent + (" ") + lastNameStudent));;
+        $(".modal-content").shouldHave(text("Student Email")).parent().shouldHave(text("testUserEmail@test.ru"));;
+        $(".modal-content").shouldHave(text("Gender")).parent().shouldHave(text("Other"));;
+        $(".modal-content").shouldHave(text("Mobile")).parent().shouldHave(text("8912251475"));;
+        $(".modal-content").shouldHave(text("Date of Birth")).parent().shouldHave(text("12 April,1939"));;
+        $(".modal-content").shouldHave(text("Subjects")).parent().shouldHave(text("Chemistry"));;
+        $(".modal-content").shouldHave(text("Hobbies")).parent().shouldHave(text("Sports, Reading, Music"));;
+        $(".modal-content").shouldHave(text("Picture")).parent().shouldHave(text("photo_2024-01-11_13-55-58.jpg"));;
+        $(".modal-content").shouldHave(text("Address")).parent().shouldHave(text("currentAddressTest"));;
+        $(".modal-content").shouldHave(text("State and City")).parent().shouldHave(text("Rajasthan Jaipur"));;
     }
 }
